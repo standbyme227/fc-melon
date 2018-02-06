0af11f6842443061b8d56aec11acc6a3ad9ec598
ShinJongMin <standbyme227@gmail.com>
2018-02-06 16:45:32 +0900

    artist모델 만듬

--- django/artist/models.py
+++ django/artist/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    BLOOD_TYPE_A = 'a'
+    BLOOD_TYPE_B = 'b'
+    BLOOD_TYPE_O = 'o'
+    BLOOD_TYPE_C = 'c'
+    BLOOD_TYPE_X = 'x'
+    CHOICES_BOOD_TYPE = (
+        (BLOOD_TYPE_A, 'A형'),
+        (BLOOD_TYPE_B, 'B형'),
+        (BLOOD_TYPE_O, 'O형'),
+        (BLOOD_TYPE_C, 'AB형'),
+        (BLOOD_TYPE_X, '기타'),
+    )
+    image = models.IntegerField('프로필 이미지', upload_to='artist', )
+    name = models.CharField('이름', max_length=50, )
+    real_name = models.CharField('본명', max_length=30, blank=True, )
+    nationality = models.CharField('국적', max_length=50, blank=True, )
+    birth_date = models.CharField(blank=True, null=True, )
+    constellation = models.CharField('별자리', max_length=30, )
+    blood_type = models.CharField('혈액형', blank=True)
+
+    intro = models.TextField('소개', blank=True)
+
+    def __str__(self):
+        return self.name
