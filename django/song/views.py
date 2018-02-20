cae6a74edca1ebceca235e6702ed2f2c6a77ad14
ShinJongMin <standbyme227@gmail.com>
2018-02-20 10:58:50 +0900

    search song을 이용해서 앨범과 아티스트에서 song을 추출함. post방식

--- django/song/views.py
+++ django/song/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -20,8 +21,12 @@ def song_search(request):
     if request.method == 'POST':
         keyword = request.POST['keyword'].strip()
         if keyword:
-            songs = Song.objects.filter(title__contains=keyword)
-            context['songs'] = songs
+            songs_from_artists = Song.objects.filter(album__artists__name__contains=keyword).distinct()
+            songs_from_albums = Song.objects.filter(album__title__contains=keyword).distinct()
+            songs_from_titles = Song.objects.filter(title__contains=keyword).distinct()
+            context['songs_from_albums'] = songs_from_albums
+            context['songs_from_artist'] = songs_from_artists
+            context['songs_from_title'] = songs_from_titles
     # 만약 method 가 POST였다면 context에 'songs'가 채워진 상태,
     # GET이면 빈 상태로 render 실행
     return render(request, 'song/song_search.html', context)
