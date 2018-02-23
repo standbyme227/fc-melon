38924955068c645186ccea72e414b757cbc9c0bb
ShinJongMin <standbyme227@gmail.com>
2018-02-23 16:54:57 +0900

    validation_error 구현

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
+User = get_user_model()
 __all__ = (
     'SignupForm'
 )
@@ -13,3 +16,16 @@ class SignupForm(forms.Form):
     password2 = forms.CharField(label='비밀번호 확인',
         widget=forms.PasswordInput
     )
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        if User.objects.filter(username=data).exists():
+            raise ValidationError('이미 사용중인 아이디입니다.')
+        return data
+
+    def clean_password2(self):
+        password = self.cleaned_data['password']
+        password2 = self.cleaned_data['password2']
+        if password != password2:
+            raise ValidationError('비밀번호가 일치하지 않습니다.')
+        return password2
--- app/members/views.py
+++ app/members/views.py
@@ -68,19 +68,10 @@ def signup_view(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            password2 = form.cleaned_data['password2']
-            is_valid=True
-            if User.objects.filter(username=username).exists():
-                form.add_error('username', '이미 사용되고 있는 아이디입니다.')
-                is_valid = False
-            if password != password2:
-                form.add_error('password2', '비밀번호와 비밀번호 확인란의 값이 다릅니다.')
-                is_valid = False
-            if is_valid:
-                User.objects.create_user(username=username, password=password)
-                user = authenticate(request, username=username, password=password)
-                login(request, user)
-                return redirect('index')
+            User.objects.create_user(username=username, password=password)
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('index')
     else:
         form = SignupForm()
     context = {
