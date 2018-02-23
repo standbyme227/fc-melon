bccff1af0bfac430c84f579d969cc15f40c6f51c
ShinJongMin <standbyme227@gmail.com>
2018-02-23 12:26:17 +0900

    database의 프로그램?을 바꿈.

--- app/config/settings.py
+++ app/config/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fc-melon',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'fc-7th',
+        'PASSWORD':'whdals1208',
     }
 }
 
